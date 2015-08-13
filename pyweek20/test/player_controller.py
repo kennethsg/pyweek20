7fbc94af6575910a968eca95570dd7e27927b414
Kenneth Solano González <kennethsolano@gmail.com>
2015-08-13 07:09:12 -0600

    not control yet, but it compiles

--- pyweek20/test/player_controller.py
+++ pyweek20/test/player_controller.py
@@ -5,6 +5,7 @@ class PlayerController(actions.Move):
     def step(self, dt):
         super(PlayerController, self).step(dt)
         global keyboard
+        keyboard = key.KeyStateHandler()
         # Determine velocity based on keyboard inputs.
         velocity_x = 100 * (keyboard[key.RIGHT] - keyboard[key.LEFT])
         velocity_y = 100 * (keyboard[key.UP] - keyboard[key.DOWN])
--- pyweek20/test/sprite1.py
+++ pyweek20/test/sprite1.py
@@ -1,27 +1,23 @@
 import cocos
 #import pyglet.window.key.KeyStateHandler
 from pyglet.window import key
+from cocos import actions
 import player_controller
+from cocos.actions.move_actions import Move
 
 class Sprite1(cocos.sprite.Sprite):
-    #def __init__(self, image, position = (...), rotation = 0, scale = 1, opacity = 255, color = (...), anchor = None):
-        #return super(sprite, self).__init__(image, position, rotation, scale, opacity, color, anchor)
-
-    #===========================================================================
-    # def __init__(self):
-    #     super(Sprite, self).__init__('rectangle_blue.png', position = (0,100), scale = 0.5, anchor = 1)
-    #===========================================================================
-
     def __init__(self):
         super(Sprite1, self).__init__('rectangle_blue.png')
 
         self.position = (0, 100)
         self.scale = 0.5
         
+        self.velocity = (0, 0)
+        #self.do(Move())
         
-        #the_player_controller = player_controller.PlayerController()
-        #self.do(the_player_controller)
+        the_player_controller = player_controller.PlayerController()
+        self.do(the_player_controller)
         
-        self.key_handler = key.KeyStateHandler()
+        #self.key_handler = key.KeyStateHandler()
 
         
